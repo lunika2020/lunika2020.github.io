40dbff787bcccbddab99b688921b0cfeec5f7b30
Lunika <541183248@qq.com>
2021-02-25 13:01:03 +0800

    czh

---
+++ music2/public.js
@@ -0,0 +1,16 @@
+export function _debounce(fn, delay) {
+ 
+    var delay = delay || 200;
+    var timer;
+    return function () {
+        var th = this;
+        var args = arguments;
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function () {
+            timer = null;
+            fn.apply(th, args);
+        }, delay);
+    };
+}
